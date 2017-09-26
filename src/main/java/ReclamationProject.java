b1da705c862596b7a15542138b73ad54c4140c7c
Gary Braznichenko <garyab2@illinois.edu>
2017-09-26 18:13:11 -0500

    Fixed all checkstyle errors and poor style.

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,35 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
-
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+/**
+ * This class contains the method common.
+ * @author - Gary Braznichenko (garyab2)
+ */
+public class ReclamationProject {
+    /**
+     *
+     * @param word1 - A word passed into the common method.
+     * @param word2 - A second word passed into the common method.
+     * @return commonString - The common substring that can be found in both word1 and word2.
+     */
+    static String common(final String word1, final String word2) {
+        //swaps longWord with shortWord if shortWord is the longer of the two
+        String longWord = "", shortWord = "";
+        if (word1.length() > word2.length()) {
+            longWord = word1;
+            shortWord = word2;
+        } else {
+            longWord = word2;
+            shortWord = word1;
+        }
+        String commonString = "";
+        for (int j = 0; j < shortWord.length(); j++) {
+            for (int k = shortWord.length(); k > j; k--) {
+                if (longWord.indexOf(shortWord.substring(j, k)) != -1) {
+                    commonString = shortWord.substring(j, k);
+                }
+            }
+        }
+        return commonString;
+     }
 }
